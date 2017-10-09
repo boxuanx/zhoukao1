cab5bda4d44b20c25dde98f91e6aa1b9b2ca721f
boxuan <394747029@qq.com>
2017-10-09 20:14:28 +0800

    123

---
+++ two.js
@@ -0,0 +1,14 @@
+//去重二
+var arr = ['0','1','2','0','7','1'];
+function unique(arr){
+    var newArr = [];
+    for(var i=0;i<arr.length;i++){
+        if(newArr.indexOf(arr[i]) == -1){
+            newArr.push(arr[i]);
+            //哼
+        }
+    }
+    return newArr;
+}
+console.log(unique(arr));
+
