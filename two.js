afcbef87d6b253cf13fb3a9e093139f6c7d9a23f
boxuan <394747029@qq.com>
2017-10-09 19:58:38 +0800

    没改

--- two.js
+++ two.js
@@ -1,8 +1,8 @@
 //去重二
 var arr = ['0','1','2','0','7','1'];
 function unique(arr){
-    var newArr = [arr[0]];
-    for(var i=1;i<arr.length;i++){
+    var newArr = [];
+    for(var i=0;i<arr.length;i++){
         if(newArr.indexOf(arr[i]) == -1){
             newArr.push(arr[i]);
         }
